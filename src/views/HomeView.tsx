3854045f1b0f2260fac1fbdfacf943e8c9d55cac
Andrew Stamm <andrewstamm@me.com>
2024-11-23 15:01:41 -0600

    Initial Data Loading

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { StyleSheet, Text, View } from "react-native";
 import { FODatePicker } from '../components';
 import { DateTimePickerEvent } from '@react-native-community/datetimepicker';
+import { NearEarthObject } from '../models';
+import { loadNEOs } from '../network';
 
 const HomeView: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+  const [nearEarthObjects, setNearEarthObjects] = useState<NearEarthObject[]>([]);
 
-  const onDateSelect = (event: DateTimePickerEvent, selectedDate: Date) => {
-    if (event.type === 'set') {
-      setSelectedDate(selectedDate);
+  const onDateSelect = useCallback((event: DateTimePickerEvent, selectedDate: Date) => {
+    setSelectedDate(selectedDate);
+  }, []);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const objects = await loadNEOs(selectedDate);
+      console.log(objects);
+      setNearEarthObjects(objects);
     }
-  }
+
+    loadData();
+  }, [selectedDate])
 
   return (
     <View style={styles.container}>
