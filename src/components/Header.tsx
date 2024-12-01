22d0e7ccf68807c0017d7538413ada04fc90bb4c
Andrew Stamm <andrewstamm@me.com>
2024-12-01 09:24:14 -0600

    Unused folder cleanup, and Header font fix for Android

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -42,7 +42,6 @@ export const styles = StyleSheet.create({
   headerText: {
     color: 'white',
     textAlign: 'center',
-    fontWeight: 'bold',
     fontSize: 48,
     textTransform: 'uppercase',
     marginBottom: 2,
